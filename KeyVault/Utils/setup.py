ddc93e756307db9b2ab1556ac5cadc1f9a34cf93
belzebug <belzebug@protonmail.com>
2021-04-16 14:12:48 +0200

    bug fix setup.py and db.py clean up

--- KeyVault/Utils/setup.py
+++ KeyVault/Utils/setup.py
@@ -5,16 +5,20 @@ print(getpass.getuser())
 def setup():
     user = getpass.getuser()
     pathVault = '/home/' + user + '/PyVault/'
-    if os.path.exists(pathVault):
-        pass
-    else:
+    if not os.path.exists(pathVault):
         os.mkdir(pathVault)
+    if not os.path.exists(pathVault + 'database/'):
         os.mkdir(pathVault + 'database/')
+        print('database conf folder has been created')
+    if not os.path.exists(pathVault + 'database/'+ user + '.db'):
         os.mknod(pathVault + 'database/' + user + '.db')
+        print('the database has been created')
+        
     print(pathVault)
 
 def main():
     setup()
+    print('the setup for PyVault is done !!')
 
 if __name__ == "__main__":
     main()
--- KeyVault/database/db.py
+++ KeyVault/database/db.py
@@ -22,22 +22,3 @@ def writeKey(name, value):
     connection.commit()
 
 
-
-con = sqlite3.connect('example.db')
-
-cur = con.cursor()
-
-# Create table
-cur.execute('''CREATE TABLE IF NOT EXISTS stocks
-               (date text, trans text, symbol text, qty real, price real)''')
-
-# Insert a row of data
-cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-# Save (commit) the changes
-con.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-print(con.execute("SELECT * FROM stocks"))
-con.close()
