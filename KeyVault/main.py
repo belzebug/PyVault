d41c9ef8dbd9cf4115d13764d5349fbd28c2a9fe
belzebug <belzebug@protonmail.com>
2021-04-16 11:45:27 +0200

    v2

--- KeyVault/main.py
+++ KeyVault/main.py
@@ -6,6 +6,7 @@ try:
     import os
 except ImportError:  # Python 3
     import tkinter as tk
+    from tkinter import *
     import tkinter.font as tkFont
     import tkinter.ttk as ttk
     import database.db as mdb
@@ -30,7 +31,7 @@ class MultiColumnListbox(object):
 
     def _setup_widgets(self):
         s = """\click on header to sort by that column
-to change width of column drag boundary
+to change width of column drag boundary and click on a row to copy the key
         """
         msg = ttk.Label(wraplength="4i", justify="left", anchor="n",
             padding=(10, 2, 10, 6), text=s)
@@ -67,6 +68,7 @@ to change width of column drag boundary
                 col_w = tkFont.Font().measure(val)
                 if self.tree.column(header[ix],width=None)<col_w:
                     self.tree.column(header[ix], width=col_w)
+        
 
 def sortby(tree, col, descending):
     """sort tree contents when a column header is clicked on"""
@@ -82,6 +84,49 @@ def sortby(tree, col, descending):
     # switch the heading so it will sort in the opposite direction
     tree.heading(col, command=lambda col=col: sortby(tree, col, \
         int(not descending)))
+
+
+
+class newWindow(tk.Toplevel):
+    def __init__(self, master = None):  
+        super().__init__(master = master)
+        self.title("New Key")
+        self.geometry("400x400")
+        #label = tk.Label(self, text ="Register new key")
+        #label.grid(column=0)
+
+        ## The key input parts
+        inputName = tk.Label(self, text="Name")
+        inputName.grid(column=0, row=0)
+        inputtxt = tk.Entry(self,
+                   width = 20, textvariable=v1)
+        inputtxt.grid(column=1, row=0)
+
+        inputValue = tk.Label(self, text="Value")
+        inputValue.grid(column=0, row=1)
+        inputtxt2 = tk.Entry(self,
+                   width = 7, textvariable=v2)
+        inputtxt2.grid(column=1, row=1)
+        #input = inputtxt.get("1.0","end-1c")
+        #input2 = inputtxt2.get("1.0","end-1c")
+        #Destroy window
+        tk.Button(self,
+                text='Close',
+                command=self.destroy).grid(column=0, row=2)
+        tk.Button(self,
+                text='Save',
+                command=self.registerNewKey).grid(column=1, row=2)
+        
+    def registerNewKey(self):
+        print(v1.get())
+        print(v2.get())
+        mdb.writeKey(v1.get(), v2.get())
+        self.destroy()
+        
+
+
+
+
 
 # the test data ...
 
@@ -99,12 +144,37 @@ print(data)
 #data = list.append()
 
 def addToClipBoard(text):
-    command = 'echo ' + text.strip() + '| xclip'
+    command = 'echo ' + text.strip() + '| xclip -selection clipboard' 
     os.system(command)
+
+
+def update():
+ root.destroy()
+ os.system('python /home/belzebug/Documents/Projects/KeyVault/KeyVault/main.py')
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.title("Multicolumn Treeview/Listbox")
+    v1 = tk.StringVar()
+    v2 = tk.StringVar()
+    root.title("PyVault")
+    root.geometry("800x600")
+    btn = tk.Button(root, text ="Create new key")
+  
+    # Following line will bind click event
+    # On any click left / right button
+    # of mouse a new window will be opened
+    btn.bind("<Button>", lambda e: newWindow(root))
+    btn.pack(pady = 10)
+
+    btn2 = tk.Button(root, text ="Update")
+  
+    # Following line will bind click event
+    # On any click left / right button
+    # of mouse a new window will be opened
+    btn2.bind("<Button>", lambda e: update())
+    btn2.pack(pady = 10)
+
     listbox = MultiColumnListbox()
+    #update()
     root.mainloop()
 
