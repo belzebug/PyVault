fdc81931b674d8083c8d0d42b1c1538cee85db3c
belzebug <belzebug@protonmail.com>
2021-04-16 14:00:50 +0200

    bug fix install.sh

--- KeyVault/install.sh
+++ KeyVault/install.sh
@@ -1,6 +1,6 @@
-sudo apt install python-tkinter
+sudo apt install python-tk
 sudo apt install xclip
 git clone https://github.com/belzebug/PyVault
 mkdir ~/PyVault
-cp -avr KeyVault ~/PyVault
+cp -avr PyVault/KeyVault ~/PyVault
 echo "alias pyvault='python ~/PyVault/KeyVault/main.py'" >> ~/.bashrc
